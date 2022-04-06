a8270ca38d55b679ced7d02558474e2a268276dd
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2022-04-06 07:19:06 +0300

    06-04

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+import random
+import telebot
+from bs4 import BeautifulSoup as b
+
+URL = 'https://www.anekdot.ru/last/good/'
+API_KEY = ''
+def parser(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    anekdots = soup.find_all('div', class_='text')
+    return [c.text for c in anekdots]
+
+list_of_jokes = parser(URL)
+random.shuffle(list_of_jokes)
+
+bot = telebot.TeleBot(API_KEY)
+@bot.message_handler(commands=['начать'])
+
+def hello(message):
+    bot.send_message(message.chat.id, 'Здравствуйте! Чтобы посмеяться введите любую цифру:')
+
+@bot.message_handler(content_types=['text'])
+def jokes(message):
+    if message.text.lower() in '123456789':
+        bot.send_message(message.chat.id, list_of_jokes[0])
+        del list_of_jokes[0]
+    else:
+        bot.send_message(message.chat.id, 'Введите любую цифру:')
+bot.polling()
